40cca1c62584783c97221faa433d63cea38bb7c1
Charles Chin <eallions@gmail.com>
2021-01-17 14:37:48 +0800

    首页嘀咕 Widget 采用 JSON 输出，减少网页体积

--- themes/hello-friend/assets/js/all.js
+++ themes/hello-friend/assets/js/all.js
@@ -43,7 +43,7 @@ jQuery(document).ready(function ($) {
   })
   $(".post-content a[rel!=link]:has(img)").slimbox();
   //相对时间
-  $.lately({ 'target': '.post-date' });
+  //$.lately({ 'target': '.post-date' });
   //文章toc固定
   var nav = $(".tocify");
   if (nav.length > 0) {
@@ -140,4 +140,31 @@ $('.to-top').toTop({
   position: true,
   right: 15,
   bottom: 30
+});
+
+// 首页调用嘀咕 JSON 版
+$(document).ready(function(){
+  if ( $("#index-talk").length > 0 ) {
+    jsonUrl = "https://6561-eallion-8gkunp4re49bae66-1251347414.tcb.qcloud.la/json/talks.json"
+    $.getJSON(jsonUrl+"?t="+Date.parse( new Date()),function(res){
+      var talksHtml = ''
+      $.each(res.data, function(i, item){
+        d = new Date(item.date)
+        date = d.getFullYear()+'/'+(d.getMonth()+1)+'/'+d.getDate() +' '+d.getHours()+':'+d.getMinutes()+':'+d.getSeconds()
+        dataTime = '<span class="datatime">'+date+'</span>'
+        talksHtml += '<li class="item item-'+(i+1)+'">'+dataTime+'： <a href="https://eallion.com/talk/" target="_blank">'+item.content+'</a></li>'
+      });
+      $('#index-talk').append('<span class="index-talk-icon"><svg height="24" width="24" viewBox="0 0 20 20"><path d="M17.659,3.681H8.468c-0.211,0-0.383,0.172-0.383,0.383v2.681H2.341c-0.21,0-0.383,0.172-0.383,0.383v6.126c0,0.211,0.172,0.383,0.383,0.383h1.532v2.298c0,0.566,0.554,0.368,0.653,0.27l2.569-2.567h4.437c0.21,0,0.383-0.172,0.383-0.383v-2.681h1.013l2.546,2.567c0.242,0.249,0.652,0.065,0.652-0.27v-2.298h1.533c0.211,0,0.383-0.172,0.383-0.382V4.063C18.042,3.853,17.87,3.681,17.659,3.681 M11.148,12.87H6.937c-0.102,0-0.199,0.04-0.27,0.113l-2.028,2.025v-1.756c0-0.211-0.172-0.383-0.383-0.383H2.724V7.51h5.361v2.68c0,0.21,0.172,0.382,0.383,0.382h2.68V12.87z M17.276,9.807h-1.533c-0.211,0-0.383,0.172-0.383,0.383v1.755L13.356,9.92c-0.07-0.073-0.169-0.113-0.27-0.113H8.851v-5.36h8.425V9.807z"></path></svg></span><ul class="talk-list">'+talksHtml+'</ul>')
+      Lately({ 'target': '#index-talk .datatime' });
+    });
+    function Roll (){
+      var list_li = $('.talk-list li'),cur_li = list_li.first(),last_li = list_li.last();
+      last_li.after(cur_li);
+    };
+    setInterval(Roll,3000);
+    //点击关闭嘀咕 Widget
+	  $('button').click(function(){
+		  $(this).parents('#index-talk').remove();
+	  });
+  }
 });
