e91a2dfe116e9bd4445d0cae63aee62eaa228298
eallion <eallion@users.noreply.github.com>
2021-03-01 06:03:56 +0800

    deploy: eallion/eallion.com@ea79bc2aab696dd63e06e09c5c95f047ebc200ed

--- js/jquery.toTop.min.ee0c6cdb4c89350f3de38c8d77aaf118ba74df4951a02a113d5aff9794a1ef33.js
+++
@@ -1,4 +0,0 @@
-(function($){'use strict';$.fn.toTop=function(opt){var elem=this;var win=$(window);var doc=$('html, body');var options=$.extend({autohide:true,offset:420,speed:500,position:true,right:15,bottom:30},opt);elem.css({'cursor':'pointer'});if(options.autohide){elem.css('display','none');}
-if(options.position){elem.css({'position':'fixed','right':options.right,'bottom':options.bottom,});}
-elem.click(function(){doc.animate({scrollTop:0},options.speed);});win.scroll(function(){var scrolling=win.scrollTop();if(options.autohide){if(scrolling>options.offset){elem.fadeIn(options.speed);}
-else elem.fadeOut(options.speed);}});};}(jQuery));
