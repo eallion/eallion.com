b82861f0c27fa9e292d8510058486a31e5432bcb
Charles Chin <eallions@gmail.com>
2021-09-07 13:14:50 +0800

    fix(coding): fix coding ci bash script
    fix:
    - curl: (22) The requested URL returned error: 404 Not Found

--- coding.sh
+++ coding.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-Version="0.88.1"
-echo ${Version}
 mkdir hugo
 cd hugo
-curl -fL "https://coding-public-generic.pkg.coding.net/public/downloads/hugo-Linux-64bit.deb?version=${Version}" -o hugo-Linux-64bit.deb
-apt-get -y install ./hugo-Linux-64bit.deb
+curl -fL "https://coding-public-generic.pkg.coding.net/public/downloads/hugo-linux-64bit.deb?version=0.88.1" -o hugo-linux-64bit-0.88.1.deb
+apt-get -y install ./hugo-linux-64bit-0.88.1.deb
 cd ..
