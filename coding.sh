032c1b23766c657a039f94e1f6b49213446c6f7d
Charles Chin <eallions@gmail.com>
2021-11-11 01:19:05 +0800

    chore(hugo): hugo version bump to 0.89.2

--- coding.sh
+++ coding.sh
@@ -2,6 +2,6 @@
 
 mkdir hugo
 cd hugo
-curl -fL "https://coding-public-generic.pkg.coding.net/public/downloads/hugo-linux-64bit.deb?version=0.89.1" -o hugo-linux-64bit-0.89.1.deb
-apt-get -y install ./hugo-linux-64bit-0.89.1.deb
+curl -fL "https://coding-public-generic.pkg.coding.net/public/downloads/hugo-linux-64bit.deb?version=0.89.2" -o hugo-linux-64bit-0.89.2.deb
+apt-get -y install ./hugo-linux-64bit-0.89.2.deb
 cd ..
