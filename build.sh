fe1371f58361886b280740e2a5db7e8d547433b1
Charles Chin <eallions@gmail.com>
2021-11-15 12:57:44 +0800

    build: update local build shell

--- build.sh
+++ build.sh
@@ -7,8 +7,11 @@ else
   echo ".................."
   echo ". Public deleted ."
   echo ".................."
+  cp themes/hello-friend/layouts/partials/githash.html themes/hello-friend/layouts/partials/githash.bak
   bash githash.sh
   hugo --cleanDestinationDir --forceSyncStatic --gc --ignoreCache --minify --enableGitInfo
+  rm themes/hello-friend/layouts/partials/githash.html
+  mv themes/hello-friend/layouts/partials/githash.bak themes/hello-friend/layouts/partials/githash.html
   echo ".............."
   echo ". Hugo built ."
   echo ".............."
