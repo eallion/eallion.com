6a6f95c4c884e788035fa92383e6049d5378c2e1
Charles Chin <eallions@gmail.com>
2022-02-14 20:19:37 +0800

    style(header): remove header sticky css style
    refactor later

--- themes/hello-friend/assets/js/all.js
+++ themes/hello-friend/assets/js/all.js
@@ -192,31 +192,3 @@ $(document).ready(function () {
         });
     }
 });
-// The debounce function receives our function as a parameter
-const debounce = (fn) => {
-    // This holds the requestAnimationFrame reference, so we can cancel it if we wish
-    let frame;
-    // The debounce function returns a new function that can receive a variable number of arguments
-    return (...params) => {
-        // If the frame variable has been defined, clear it now, and queue for next frame
-        if (frame) {
-            cancelAnimationFrame(frame);
-        }
-        // Queue our function call for the next frame
-        frame = requestAnimationFrame(() => {
-            // Call our function and pass any params we received
-            fn(...params);
-        });
-    }
-};
-// Reads out the scroll position and stores it in the data attribute
-// so we can use it in our stylesheets
-const storeScroll = () => {
-    document.documentElement.dataset.scroll = window.scrollY;
-}
-// Listen for new scroll events, here we debounce our `storeScroll` function
-document.addEventListener('scroll', debounce(storeScroll), {
-    passive: true
-});
-// Update scroll position for first time
-storeScroll();
