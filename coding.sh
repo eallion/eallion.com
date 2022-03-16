e6b3ba2ce27df98c3f7c1892ed8925f915038ced
Charles Chin <eallions@gmail.com>
2022-03-17 01:23:46 +0800

    chore(hugo): hugo version bump to 0.95.0

--- coding.sh
+++ coding.sh
@@ -2,6 +2,6 @@
 
 mkdir hugo
 cd hugo
-curl -fL "https://coding-public-generic.pkg.coding.net/public/downloads/hugo-linux-64bit.deb?version=0.94.2" -o hugo-linux-64bit-0.94.2.deb
-apt-get -y install ./hugo-linux-64bit-0.94.2.deb
+curl -fL "https://coding-public-generic.pkg.coding.net/public/downloads/hugo-linux-64bit.deb?version=0.95.0" -o hugo-linux-64bit-0.95.0.deb
+apt-get -y install ./hugo-linux-64bit-0.95.0.deb
 cd ..
