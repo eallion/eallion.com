d320e8bdac25b1048f20fa7453cd76f9293dc7e3
Charles Chin <eallions@gmail.com>
2022-04-23 09:22:02 +0800

    chore: update server.sh

--- server.sh
+++ server.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-start chrome http://192.168.0.5:8080
+# 优先运行 Firefox-devlopper
+if [ `command -v firefox-dev` ]; then
+    start firefox-dev http://192.168.0.5:8080
+else
+    start chrome http://192.168.0.5:8080
+fi
 hugo server -w -D -p 8080 -t hello-friend --bind 192.168.0.5 --baseURL 192.168.0.5
