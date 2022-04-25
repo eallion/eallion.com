84d1b3a61d755257558499b6bf8b7d34a4777628
Charles Chin <eallions@gmail.com>
2022-04-25 19:34:11 +0800

    perf: rm unuse assets

--- themes/hello-friend/assets/js/all.js
+++ themes/hello-friend/assets/js/all.js
@@ -118,16 +118,6 @@ jQuery(document).ready(function ($) {
         }
     });
 });
-// 回到顶部
-$('.to-top').toTop({
-    //options with default values
-    autohide: true,
-    offset: 420,
-    speed: 500,
-    position: true,
-    right: 15,
-    bottom: 30
-});
 // 首页调用嘀咕 JSON 版
 $(document).ready(function () {
     if ($("#index-talk").length > 0) {
@@ -635,3 +625,142 @@ function switchDarkMode(e) {
         document.body.classList.remove('dark')
     }
 }
+/**
+ * Lately.js - Native JavaScript, only 800Byte but simple and easy to use Timeago plugin
+ *
+ * @name Lately.js
+ * @version 2.0.1
+ * @author Tokin (Tokinx)
+ * @license MIT License - http://www.opensource.org/licenses/mit-license.php
+ *
+ * For usage and examples, visit:
+ * https://tokinx.github.io/lately/
+ *
+ * Copyright (c) 2017, Biji.IO
+ */
+ ; (function (global, undefined) {
+    "use strict"
+    let _global;
+    let Lately = (option) => {
+        let target = option.target || ".time";
+        let lang = option.lang || {
+            'second': '秒',
+            'minute': '分钟',
+            'hour': '小时',
+            'day': '天',
+            'month': '个月',
+            'year': '年',
+            'ago': '前',
+            'error': 'NaN',
+        };
+        let _count = (date) => {
+            date = new Date(date);
+            let second = (new Date().getTime() - date.getTime()) / 1000,
+                minute = second / 60,
+                hour = minute / 60,
+                day = hour / 24,
+                month = day / 30,
+                year = month / 12,
+                floor = (num, _lang) => Math.floor(num) + _lang,
+                result = '';
+            if (year >= 1) result = floor(year, lang.year);
+            else if (month >= 1) result = floor(month, lang.month);
+            else if (day >= 1) result = floor(day, lang.day);
+            else if (hour >= 1) result = floor(hour, lang.hour);
+            else if (minute >= 1) result = floor(minute, lang.minute);
+            else if (second >= 1) result = floor(second, lang.second);
+            else result = lang.error;
+            return result + lang.ago;
+        }
+        for (let contain of document.querySelectorAll(target)) {
+            let date = '',
+                date_time = contain.dateTime,
+                title = contain.title,
+                html = contain.innerHTML;
+            if (date_time ? !isNaN(new Date(date_time = (date_time.replace(/(.*)[a-z](.*)\+(.*)/gi, "$1 $2")).replace(/-/g, "/"))) : false) date = date_time;
+            else if (title ? !isNaN(new Date(title = title.replace(/-/g, "/"))) : false) date = title;
+            else if (html ? !isNaN(new Date(html = html.replace(/-/g, "/"))) : false) date = html;
+            else return;
+            contain.title = date;
+            contain.innerHTML = _count(date);
+        }
+    }
+
+    _global = (function () { return this || (0, eval)('this'); }());
+    !('Lately' in _global) && (_global.Lately = Lately);
+}());
+/**
+ *  Plugin Name: jQuery toTop for smoothly Scroll back to Top
+ *  Plugin URL: https://github.com/mmkjony/jQuery.toTop
+ *  Version: 1.1
+ *  Author: MMK Jony
+ *  Author URL: https://github.com/mmkjony
+ *  License: Licensed under MIT
+ **/
+
+ (function( $ ){
+    'use strict';
+
+    $.fn.toTop = function(opt){
+
+        //variables
+        var elem = this;
+        var win = $(window);
+        var doc = $('html, body');
+
+        //Extended Options
+        var options = $.extend({
+            autohide: true,
+            offset: 420,
+            speed: 500,
+            position: true,
+            right: 15,
+            bottom: 30
+        }, opt);
+
+        elem.css({
+            'cursor': 'pointer'
+        });
+
+        if(options.autohide){
+            elem.css('display', 'none');
+        }
+
+        if(options.position){
+            elem.css({
+                'position': 'fixed',
+                'right': options.right,
+                'bottom': options.bottom,
+            });
+        }
+
+        elem.click(function(){
+            doc.animate({scrollTop: 0}, options.speed);
+        });
+
+        win.scroll(function(){
+            var scrolling = win.scrollTop();
+
+            if(options.autohide){
+                if(scrolling > options.offset){
+                    elem.fadeIn(options.speed);
+                }
+                else elem.fadeOut(options.speed);
+            }
+
+        });
+
+    };
+
+}( jQuery ));
+
+// 回到顶部
+$('.to-top').toTop({
+    //options with default values
+    autohide: true,
+    offset: 420,
+    speed: 500,
+    position: true,
+    right: 15,
+    bottom: 30
+});
