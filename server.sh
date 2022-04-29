5bff1318f5d69e32e2beb9b70cdda9c37cd370e6
Charles Chin <eallions@gmail.com>
2022-04-30 01:22:23 +0800

    chore: update server.sh

--- server.sh
+++ server.sh
@@ -2,8 +2,8 @@
 
 # 优先运行 Firefox-devlopper
 if [ `command -v firefox-dev` ]; then
-    start firefox-dev http://192.168.0.5:8080
+    start firefox-dev -private-window "http://192.168.0.5:8080"
 else
-    start chrome http://192.168.0.5:8080
+    start chrome -incognito "http://192.168.0.5:8080"
 fi
-hugo server -w -D -p 8080 -t hello-friend --bind 192.168.0.5 --baseURL 192.168.0.5
+hugo server -w -D --cleanDestinationDir --forceSyncStatic --ignoreCache --noHTTPCache -p 8080 -t hello-friend --bind 192.168.0.5 --baseURL 192.168.0.5
