192529d5cba77e5946c2d98435e525fd0585a1e7
Charles Chin <eallions@gmail.com>
2022-11-23 22:27:31 +0800

    chore: revert server shell script

--- server.sh
+++ server.sh
@@ -6,5 +6,4 @@ if [ `command -v firefox-dev` ]; then
 else
     start chrome -incognito "http://127.0.0.1:1313"
 fi
-(npm run dev&)
 hugo server -w -D -p 1313 -t hello-friend --bind 127.0.0.1 --baseURL 127.0.0.1 --contentDir example --forceSyncStatic --ignoreCache --noHTTPCache
