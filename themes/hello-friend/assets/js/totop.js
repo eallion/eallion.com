f374d9a62a3acdcd0c51e071c7c8966a38b92935
Charles Chin <eallions@gmail.com>
2022-11-30 05:24:58 +0800

    perf: perf scroll to top style fix umami update
    * style: fix a code syntax highlight
    
    * chore: npm update
    
    * chore: update umami url
    
    * style: memos img max-height
    
    * style: scroll to top button
    
    * feat: support highlight shortcode
    
    * style: fix some darkmode color

--- themes/hello-friend/assets/js/totop.js
+++ themes/hello-friend/assets/js/totop.js
@@ -1,15 +1,41 @@
-// Go to top button
-(function (document) {
-    const topButton = document.getElementById("topControl");
-    topButton.onclick = function (e) {
-        window.scrollTo({ top: 0, behavior: "smooth" });
-    };
+const scrollTopBtn = document.querySelector('.js-scroll-top');
+if (scrollTopBtn) {
+  scrollTopBtn.onclick = () => {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+  }
 
-    window.onscroll = function () {
-        if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-            topButton.style.opacity = "1";
-        } else {
-            topButton.style.opacity = "0";
-        }
-    };
-})(document);
+  const progressPath = document.querySelector('.scroll-top path');
+  const pathLength = progressPath.getTotalLength();
+  progressPath.style.transition = progressPath.style.WebkitTransition = 'none';
+  progressPath.style.strokeDasharray = `${pathLength} ${pathLength}`;
+  progressPath.style.strokeDashoffset = pathLength;
+  progressPath.getBoundingClientRect();
+  progressPath.style.transition = progressPath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';
+  const updateProgress = function() {
+    const scroll = window.scrollY || window.scrollTopBtn || document.documentElement.scrollTopBtn;
+
+    const docHeight = Math.max(
+      document.body.scrollHeight, document.documentElement.scrollHeight,
+      document.body.offsetHeight, document.documentElement.offsetHeight,
+      document.body.clientHeight, document.documentElement.clientHeight
+    );
+
+    const windowHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+
+    const height = docHeight - windowHeight;
+    var progress = pathLength - (scroll * pathLength / height);
+    progressPath.style.strokeDashoffset = progress;
+  }
+
+  updateProgress();
+  const offset = 100;
+
+  window.addEventListener('scroll', function(event) {
+    updateProgress();
+
+    //Scroll back to top
+    const scrollPos = window.scrollY || window.scrollTopBtn || document.getElementsByTagName('html')[0].scrollTopBtn;
+    scrollPos > offset ? scrollTopBtn.classList.add('is-active') : scrollTopBtn.classList.remove('is-active');
+
+  }, false);
+}
