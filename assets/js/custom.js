bc76c0dba197e95d4ad969c459baa4bbc4cbc1c3
Charles Chin <eallions@gmail.com>
2022-12-13 00:15:20 +0800

    fix: fix errors
    * fix: code bg
    * fix: jinrishici error
    * fix: fix links
    * feat: frontmatter lastmod
    * chore: keybase proof
    * style: memos douban style
    * style: content detail
    * feat: frontmatter lastmod
    * docs: add lastmod to articles

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,13 +1,18 @@
 // 今日诗词
+var gushici = document.getElementById('gushici');
 var xhr = new XMLHttpRequest();
 xhr.open('get', 'https://api.eallion.com/jinrishici/one.json', true);
 xhr.onreadystatechange = function () {
     if (xhr.readyState === 4) {
         var result = JSON.parse(xhr.responseText);
-        var gushici = document.getElementById('gushici');
         var poem_info = document.getElementById('poem_info');
         gushici.innerHTML = '「<a href="https://www.google.com/search?q=' + result.data.content + '" target="_blank" rel="noopener noreferrer">' + result.data.content + '</a>」';
         poem_info.innerHTML = '<a href="https://www.google.com/search?q=' + result.data.origin.author + ' ' + result.data.origin.title + '" target="_blank" rel="noopener noreferrer">' + '【' + result.data.origin.dynasty + '】' + result.data.origin.author + '《' + result.data.origin.title + '》' + '</a>';
     }
 };
-xhr.send();
+
+if (!gushici) {
+    // console.log(error)
+} else {
+    xhr.send();
+}
