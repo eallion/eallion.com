13e4056e6f51599dd1f24b4348229dd606250dcd
Charles Chin <eallions@gmail.com>
2023-03-06 12:30:49 +0800

    chore: fix format shell script

--- example/posts/format_filename.sh
+++ example/posts/format_filename.sh
@@ -2,7 +2,9 @@
 # extracts date from file in the format
 # date: 2013-08-23 14:52:59
 # and prepends this to the filename
-cd example/posts
+
+# cd example/posts
+
 for f in *.md; do
     c="$(grep -Eo 'date:\W(....-..-..)' $f | cut -d: -f2 | xargs)"
     s="$(grep -Eo 'slug:\W\"(.+)' $f | cut -d'"' -f2 | xargs)"
